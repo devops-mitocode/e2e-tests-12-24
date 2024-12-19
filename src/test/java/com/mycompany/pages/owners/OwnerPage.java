8bfdf6a3afeab7e5d5d62a883bf75aa174b11d1e
dany.cenas <dany.cenas@bbva.com>
2024-12-19 15:01:25 -0500

    .

--- src/test/java/com/mycompany/pages/owners/OwnerPage.java
+++ src/test/java/com/mycompany/pages/owners/OwnerPage.java
@@ -115,7 +115,7 @@ public class OwnerPage extends PageObject {
 
     private List<WebElementFacade> getTableRows() {
 //        waitFor(ownersTable).shouldBeVisible();
-        waitFor(ownersTable).withTimeoutOf(Duration.ofSeconds(20)).shouldBeVisible();
+//        waitFor(ownersTable).withTimeoutOf(Duration.ofSeconds(20)).shouldBeVisible();
         return ownersTable.thenFindAll("tbody tr");
     }
 }
